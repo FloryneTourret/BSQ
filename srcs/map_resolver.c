cd31b37577dce07c3898f80051e5234b647baec9
Floryne Tourret <ftourret@z4r8p2.le-101.fr>
2018-07-24 19:55:27 +0200

    test de resolution

--- srcs/map_resolver.c
+++ srcs/map_resolver.c
@@ -6,7 +6,7 @@
 /*   By: ftourret <ftourret@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/24 16:49:59 by ftourret     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/24 18:33:30 by ftourret    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/24 19:52:26 by ftourret    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -26,19 +26,26 @@ void	ft_resolve_map(t_map *map)
 	{
 		ft_putnbr(square);
 		ft_putchar('\n');
-		while (map->tab[i][j] != '\n')
+		while ((i + square) <= map->height)
 		{
-			if (map->tab[i][j] == map->obs)
-				j++;
-			if (map->tab[i][j] == map->empty)
+			while ((map->tab[i][j] != '\n') && (j + square) <= map->width)
 			{
-				if (ft_check_square(map, i, j, square) == 0)
-					ft_display_map(map);
-				j++;
+				if (map->tab[i][j] == map->obs)
+					j++;
+				if (map->tab[i][j] == map->empty)
+				{
+					if (ft_check_square(map, i, j, square) == 0)
+					{
+						ft_putstr("test");
+						ft_display_map(map);
+					}
+					j++;
+				}
 			}
+			j = 0;
+			i++;
 		}
-		j = 0;
-		i++;
+		i = 0;
 		square--;
 	}
 }
@@ -48,8 +55,8 @@ int		ft_check_square(t_map *map, int i, int j, int square)
 	int	limit_i;
 	int limit_j;
 
-	limit_i = j + square - 1;
-	limit_j = i + square - 1;
+	limit_i = i + square - 1;
+	limit_j = j + square - 1;
 	while (i <= limit_i)
 	{
 		while (j <= limit_j)
@@ -57,7 +64,10 @@ int		ft_check_square(t_map *map, int i, int j, int square)
 			if (map->tab[i][j] == map->empty)
 				j++;
 			if (map->tab[i][j] == map->obs)
+			{
+				ft_putstr("KO ");
 				return (1);
+			}
 			if (j == limit_j)
 			{
 				j = 0;
@@ -74,8 +84,8 @@ void	ft_square_found(t_map *map, int limit_i, int limit_j, int square)
 	int	i;
 	int j;
 
-	i = limit_j - square + 1;
-	j = limit_i - square + 1;
+	i = limit_i - (square + 1);
+	j = limit_j - (square + 1);
 	ft_putstr("ligne : ");
 	ft_putnbr(i);
 	ft_putstr("\ncolonne : ");
